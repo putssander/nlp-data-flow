48305c5ca0c42da40d5cb12e349a24399e1925ab
Sander Puts <putssander@gmail.com>
2020-02-21 22:46:56 +0100

    spacy json logged

--- template/polyglot-python-processor-spacy/python_processor.py
+++ template/polyglot-python-processor-spacy/python_processor.py
@@ -2,11 +2,15 @@
 
 import os
 import sys
+import json
+import spacy
+
 
 from kafka import KafkaConsumer, KafkaProducer
 from util.http_status_server import HttpHealthServer
 from util.task_args import get_kafka_binder_brokers, get_input_channel, get_output_channel, get_reverse_string
 from time import sleep
+
 
 print("ENV", os.environ, flush=True)
 
@@ -14,19 +18,20 @@ print("ENV", os.environ, flush=True)
 # sleep(60)
 # print("awake", flush=True)
 
-import spacy
-nlp = spacy.load("en_core_web_sm")
+
 
 consumer = KafkaConsumer(get_input_channel(), bootstrap_servers=[get_kafka_binder_brokers()])
 producer = KafkaProducer(bootstrap_servers=[get_kafka_binder_brokers()])
 
 HttpHealthServer.run_thread()
+nlp = spacy.load("en_core_web_sm")
 
 while True:
     for message in consumer:
 
         doc = nlp(message.value.decode('utf-8'))
-        output_message = "".join(doc.to_json()).encode('utf-8')
-
+        output_message = json.dumps(doc.to_json())
+        print(output_message)
+        output_message = output_message.encode('utf-8')
         producer.send(get_output_channel(), output_message)
 
